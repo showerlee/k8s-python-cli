03783ed350dd2b4fd52227f52c4ebaacdf1bc01c
showerlee <showerlee@vip.qq.com>
2018-10-24 18:18:11 +0800

    Update k8s_create_deployment.py

--- k8s_create_deployment.py
+++ k8s_create_deployment.py
@@ -1,15 +1,11 @@
 from os import path
-
 import yaml
-
 from kubernetes import client, config
-
 import k8s_auth_v1beta1
 
+
 def main():
-
     api = k8s_auth_v1beta1.login()
-
     with open(path.join(path.dirname(__file__), "nginx-deployment.yaml")) as f:
         dep = yaml.load(f)
         resp = api.create_namespaced_deployment(
